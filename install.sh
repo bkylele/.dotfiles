e3aae9861f150afc4500373ee7e5921e6f4cb363
Brian Le <bkylele@gmail.com>
2025-05-31 01:08:33 -0700

    updated install script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+if [ $(id -u) -ne 0 ]; then
+    echo "Please run this script as root"
+    exit 1
+fi
+
+# Update packages
+sudo pacman -Syuq --noconfirm
+
+# Essentials
+sudo pacman -S bash-completion git vim yazi zoxide fastfetch less nix \
+    ttf-jetbrains-mono ttf-jetbrains-mono-nerd noto-fonts noto-fonts-cjk \
+    noto-fonts-emoji noto-fonts-extra ttf-nerd-fonts-symbols \
+    ttf-nerd-fonts-symbols-common ttf-nerd-fonts-symbols-mono \
+    pulseaudio pulsemixer bluez bluetui grim wl-clipboard brightnessctl \
+    wayland sway waybar swaybg swaylock \
+    firefox discord
+
+user_home=/home/$SUDO_USER
+
+# Create symlinks
+rm -rf $user_home/.config/
+rm -rf $user_home/.bashrc
+rm -rf $user_home/.bash_profile
+
+ln -sf $user_home/.dotfiles/config/  $user_home/.config
+ln -sf $user_home/.bashrc            $user_home/.dotfiles/config/.bashrc
+ln -sf $user_home/.bash_profile      $user_home/.dotfiles/config/.bash_profile
