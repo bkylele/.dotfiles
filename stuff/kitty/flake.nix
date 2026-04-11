75e6e29e4c6c2ec3f7113c6e20b65fa6c5aa2e7a
Brian Le <brian.k.le@proton.me>
2026-04-11 12:18:06 -0700

    Add kitty

---
+++ stuff/kitty/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "My Kitty Configuration";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.symlinkJoin {
+          name = "kitty";
+          paths = [ pkgs.kitty ];
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/kitty \
+                --add-flags "--config=${./kitty.conf}"
+          '';
+        };
+      }
+    );
+}
